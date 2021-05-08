9dc150d998fe27c29e1a5ce3618a807de2fa5523
CoderTayrone <natsugamer21@gmail.com>
2021-05-08 15:15:03 -0300

    firstr Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+const generatePokemonPromises = () => Array(150).fill().map((_, index) => fetch(getPokemonUrl(index + 1)).then(response => response.json()));
+
+ 
+const generateHTML = pokemons => {
+
+    return pokemons.reduce((accumulator, { name, id, types }) => {
+        const ElementTypes = types.map(typeInfo => typeInfo.type.name );
+
+        accumulator += `
+        <li class="card ${ElementTypes[0]}">
+            <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png">
+            <h2 class="card-title">${id} ${name}</h2>
+            <p class="card-subtitle">${ElementTypes.join(' | ')} </p>
+        </li>
+    `;
+        return accumulator
+    }, '')
+    
+};
+
+const insertPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]');
+    ul.innerHTML = pokemons;
+};
+
+
+    
+const pokemonPromises = generatePokemonPromises();
+
+Promise.all(pokemonPromises)
+.then(generateHTML)
+.then(insertPokemonsIntoPage);
+
+   
+
+
+
+
+
+
+
+
+
+
+
+ // fetch(getPokeminUrl)
+    // .then(response => response.json())
+    // .then(pokemon => {
+    //     console.log(pokemon)
+    // })
